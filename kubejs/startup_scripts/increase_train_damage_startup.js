135dba01e84b59045efc094c4fb986f83ced432b
SageofTurtles <mkern1200@gmail.com>
2025-04-13 22:47:31 -0500

    Increased damage dealt by train collisions

---
+++ kubejs/startup_scripts/increase_train_damage_startup.js
@@ -0,0 +1,12 @@
+ForgeEvents.onEvent('net.minecraftforge.event.entity.living.LivingHurtEvent', event => {
+  global.livinghurt(event)
+})
+/**
+* 
+* @param {Internal.LivingHurtEvent} event 
+*/
+global.livinghurt = event => {
+  const { amount, source: { actual }, source } = event
+  if (source.getType() != 'create.run_over') return
+  event.amount = amount*3
+}
