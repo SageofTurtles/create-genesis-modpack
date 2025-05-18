1f83046b1e3b60555d4a2ae426c003fd507d59dc
SageofTurtles <mkern1200@gmail.com>
2025-05-18 16:07:29 -0500

    Revised configs

--- kubejs/server_scripts/tooltip_tags_server.js
+++ kubejs/server_scripts/tooltip_tags_server.js
@@ -10,6 +10,7 @@ ITEMS:
 - shaft
 - clutch
 - redstone
+- oak boat
 */
 
 // UNCRAFTABLE
@@ -18,7 +19,7 @@ ServerEvents.tags('item', event => {
     'minecraft:bedrock'
   ]
   uncraftable.forEach(itemInstance => {
-    event.add('genesis:uncraftable', `${itemInstance}`)
+    event.add('genesis:uncraftable_tooltip', `${itemInstance}`)
   })
 })
 
@@ -28,7 +29,7 @@ ServerEvents.tags('item', event => {
     'minecraft:iron_sword'
   ]
   equipment.forEach(itemInstance => {
-    event.add('genesis:equipment', `${itemInstance}`)
+    event.add('genesis:equipment_tooltip', `${itemInstance}`)
   })
 })
 
@@ -40,7 +41,7 @@ ServerEvents.tags('item', event => {
     'minecraft:carrot'
   ]
   food.forEach(itemInstance => {
-    event.add('genesis:food', `${itemInstance}`)
+    event.add('genesis:food_tooltip', `${itemInstance}`)
   })
 })
 
@@ -51,7 +52,7 @@ ServerEvents.tags('item', event => {
     'minecraft:enchanted_golden_apple'
   ]
   magical.forEach(itemInstance => {
-    event.add('genesis:magical', `${itemInstance}`)
+    event.add('genesis:magical_tooltip', `${itemInstance}`)
   })
 })
 
@@ -62,7 +63,7 @@ ServerEvents.tags('item', event => {
     'minecraft:carrot'
   ]
   plantable.forEach(itemInstance => {
-    event.add('genesis:plantable', `${itemInstance}`)
+    event.add('genesis:plantable_tooltip', `${itemInstance}`)
   })
 })
 
@@ -73,7 +74,7 @@ ServerEvents.tags('item', event => {
     'create:clutch'
   ]
   rotational.forEach(itemInstance => {
-    event.add('genesis:rotational', `${itemInstance}`)
+    event.add('genesis:rotational_tooltip', `${itemInstance}`)
   })
 })
 
@@ -84,6 +85,16 @@ ServerEvents.tags('item', event => {
     'create:clutch'
   ]
   redstone.forEach(itemInstance => {
-    event.add('genesis:redstone', `${itemInstance}`)
+    event.add('genesis:redstone_tooltip', `${itemInstance}`)
+  })
+})
+
+// TRANSPORTATION
+ServerEvents.tags('item', event => {
+  let transportation = [
+    'minecraft:oak_boat'
+  ]
+  transportation.forEach(itemInstance => {
+    event.add('genesis:transportation_tooltip', `${itemInstance}`)
   })
 })
