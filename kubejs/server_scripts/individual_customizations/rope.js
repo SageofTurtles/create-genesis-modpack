568a735714a52dd757b5fd7939efe7a954389631
SageofTurtles <mkern1200@gmail.com>
2025-05-28 21:00:41 -0500

    Removed Supplementaries rope
    Replaced all recipes, tags, and loot with Farmer's Delight rope for smoother integration.

---
+++ kubejs/server_scripts/individual_customizations/rope.js
@@ -0,0 +1,32 @@
+ServerEvents.recipes(event => {
+  event.remove([
+    {input: 'supplementaries:rope'},
+    {output: 'supplementaries:rope'},
+    {id: 'farmersdelight:rope'}
+  ])
+  function ropeRecipe(count, input) {
+    event.shaped(
+      Item.of('farmersdelight:rope', count),
+      [
+        '1',
+        '1',
+        '1'
+      ],
+      {
+        1: input
+      }
+    )
+  }
+  ropeRecipe(2, 'minecraft:string')
+  ropeRecipe(3, 'supplementaries:flax')
+  ropeRecipe(2, 'farmersdelight:straw')
+})
+ServerEvents.tags('item', event => {
+  event.removeAllTagsFrom('supplementaries:rope')
+  event.add('forge:ropes', 'farmersdelight:rope')
+})
+ServerEvents.tags('block', event => {
+  event.removeAllTagsFrom('supplementaries:rope')
+  event.add('forge:ropes', 'farmersdelight:rope')
+  event.add('supplementaries:fast_fall_climbable', 'farmersdelight:rope')
+})
