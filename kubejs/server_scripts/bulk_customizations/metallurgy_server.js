47bb762eb36370d3e4af67fcc017f4984225f566
SageofTurtles <mkern1200@gmail.com>
2025-05-31 19:51:11 -0500

    Removed Create: Metalwork recipes
    These will be replaced with custom-made molten metal recipes after Create: Big Cannons is added, due to different systems being used by each mod.

--- kubejs/server_scripts/bulk_customizations/metallurgy_server.js
+++ kubejs/server_scripts/bulk_customizations/metallurgy_server.js
@@ -1,3 +1,8 @@
+// Remove default Create: Metalwork recipes
+ServerEvents.recipes(event => {
+    event.remove({ mod: 'createmetalwork' })
+})
+
 // Add item tags to Create: Ironworks crushed raw tin
 ServerEvents.tags('item', event => {
     event.add('forge:dusts/tin', 'create_ironworks:crushed_raw_tin')
