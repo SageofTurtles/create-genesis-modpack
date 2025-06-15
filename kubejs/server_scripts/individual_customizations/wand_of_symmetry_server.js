97bdf865df1e482a10aed942ade0a7a2823d20c4
SageofTurtles <mkern1200@gmail.com>
2025-06-14 19:28:09 -0500

    Tweaked crafting recipes

---
+++ kubejs/server_scripts/individual_customizations/wand_of_symmetry_server.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+  event.replaceInput(
+    { id: 'create:mechanical_crafting/wand_of_symmetry' },
+    '#forge:glass',
+    '#luminousblocks:luminous_glass_blocks'
+  )
+})
