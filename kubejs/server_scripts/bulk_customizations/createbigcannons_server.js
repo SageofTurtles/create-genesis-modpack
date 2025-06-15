535d607fa3adea5be9c361c9bb9a6a74d46f14aa
SageofTurtles <mkern1200@gmail.com>
2025-06-14 19:28:31 -0500

    More customized CBC recipes

--- kubejs/server_scripts/bulk_customizations/createbigcannons_server.js
+++ kubejs/server_scripts/bulk_customizations/createbigcannons_server.js
@@ -34,4 +34,27 @@ ServerEvents.recipes(event => {
     }
   ).id('createbigcannons:worm')
 
+  // Mirrored recipe for Cannon Welder
+  event.remove({ id: 'createbigcannons:cannon_welder_mirrored' })
+
+  // Machine Gun Round
+  event.replaceInput(
+    { id: 'createbigcannons:machine_gun_round' },
+    'create:copper_nugget',
+    'createbigcannons:cast_iron_nugget'
+  )
+
+  // Empty Powder Charge
+  event.replaceInput(
+    { id: 'createbigcannons:empty_powder_charge' },
+    '#minecraft:wool',
+    'farmersdelight:canvas'
+  )
+
+  // Cartridge Sheets
+  event.remove({ id: 'createbigcannons:big_cannon_sheet_inexpensive' })
+  event.remove({ id: 'createbigcannons:cutting/autocannon_cartridge_sheet_copper' })
+  event.remove({ id: 'createbigcannons:cutting/autocannon_cartridge_sheet_iron' })
+  event.remove({ id: 'createbigcannons:cutting/autocannon_cartridge_sheet_gold' })
+
 })
