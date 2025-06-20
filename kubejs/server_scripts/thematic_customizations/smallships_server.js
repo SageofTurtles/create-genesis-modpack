ea9525d34cda3ea4af0f024fefaabda6e7610e77
SageofTurtles <mkern1200@gmail.com>
2025-06-20 00:26:46 -0500

    Updated Small Ships custom recipes

--- kubejs/server_scripts/thematic_customizations/smallships_server.js
+++ kubejs/server_scripts/thematic_customizations/smallships_server.js
@@ -1,7 +1,12 @@
 ServerEvents.recipes(event => {
   event.replaceInput(
-    { type: 'minecraft:crafting_shaped', id: /smallships:.*/},
+    { type: 'minecraft:crafting_shaped', id: /smallships:.*/ },
     'minecraft:lead',
+    'comforts:rope_and_nail'
+  )
+  event.replaceInput(
+    { type: 'minecraft:crafting_shaped', id: /smallships:.*/ },
+    'minecraft:string',
     'farmersdelight:rope'
   )
   event.replaceOutput(
@@ -9,4 +14,16 @@ ServerEvents.recipes(event => {
     'smallships:bamboo_cog',
     'smallships:bamboo_galley'
   )
+  event.shaped(
+    Item.of('smallships:cannon_ball', 1),
+    [
+      ' I ',
+      'IGI',
+      ' I '
+    ],
+    {
+      I: 'create:iron_sheet',
+      G: 'minecraft:gunpowder'
+    }
+  ).id('smallships:cannon_ball')
 })
