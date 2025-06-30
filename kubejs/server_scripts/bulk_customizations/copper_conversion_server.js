4406fa2c179cc231aa3bf13491fa665218cc94c1
SageofTurtles <mkern1200@gmail.com>
2025-06-29 23:19:55 -0500

    Finished copper oxidizing & waxing recipes

--- kubejs/server_scripts/bulk_customizations/copper_conversion_server.js
+++ kubejs/server_scripts/bulk_customizations/copper_conversion_server.js
@@ -24,24 +24,16 @@ ServerEvents.recipes(event => {
   ]
 
   oxidizationSets.forEach(set => {
-    // Remove waxing by deployer recipes (prevents duplicate recipes)
+    // Remove waxing & scraping by deployer recipes (prevents duplicate recipes)
     event.remove([
-      {
-        type: 'create:deploying',
-        output: `${set.modid}:waxed_${set.stage1}`
-      },
-      {
-        type: 'create:deploying',
-        output: `${set.modid}:waxed_${set.stage2}`
-      },
-      {
-        type: 'create:deploying',
-        output: `${set.modid}:waxed_${set.stage3}`
-      },
-      {
-        type: 'create:deploying',
-        output: `${set.modid}:waxed_${set.stage4}`
-      }
+      { type: 'create:deploying', output: `${set.modid}:waxed_${set.stage1}` },
+      { type: 'create:deploying', output: `${set.modid}:waxed_${set.stage2}` },
+      { type: 'create:deploying', output: `${set.modid}:waxed_${set.stage3}` },
+      { type: 'create:deploying', output: `${set.modid}:waxed_${set.stage4}` },
+      { type: 'create:deploying', output: `${set.modid}:${set.stage1}` },
+      { type: 'create:deploying', output: `${set.modid}:${set.stage2}` },
+      { type: 'create:deploying', output: `${set.modid}:${set.stage3}` },
+      { type: 'create:deploying', output: `${set.modid}:${set.stage4}` }
     ])
 
     // Add waxing by deployer recipes
@@ -73,5 +65,98 @@ ServerEvents.recipes(event => {
         'minecraft:honeycomb_block'
       ]
     ).keepHeldItem()
+
+    // Add scraping by Farmer's Delight cutting recipes
+    event.custom({
+      "type": "farmersdelight:cutting",
+      "ingredients": [{ "item": `${set.modid}:waxed_${set.stage1}` }],
+      "result": [{ "item": `${set.modid}:${set.stage1}` }],
+      "tool": {
+        "type": "farmersdelight:tool_action",
+        "action": "axe_dig"
+      }
+    })
+    event.custom({
+      "type": "farmersdelight:cutting",
+      "ingredients": [{ "item": `${set.modid}:waxed_${set.stage2}` }],
+      "result": [{ "item": `${set.modid}:${set.stage2}` }],
+      "tool": {
+        "type": "farmersdelight:tool_action",
+        "action": "axe_dig"
+      }
+    })
+    event.custom({
+      "type": "farmersdelight:cutting",
+      "ingredients": [{ "item": `${set.modid}:waxed_${set.stage3}` }],
+      "result": [{ "item": `${set.modid}:${set.stage3}` }],
+      "tool": {
+        "type": "farmersdelight:tool_action",
+        "action": "axe_dig"
+      }
+    })
+    event.custom({
+      "type": "farmersdelight:cutting",
+      "ingredients": [{ "item": `${set.modid}:waxed_${set.stage4}` }],
+      "result": [{ "item": `${set.modid}:${set.stage4}` }],
+      "tool": {
+        "type": "farmersdelight:tool_action",
+        "action": "axe_dig"
+      }
+    })
+
+    // Add oxidizing by washing recipes
+    event.recipes.create.splashing(
+      `${set.modid}:${set.stage4}`,
+      `${set.modid}:${set.stage3}`
+    )
+    event.recipes.create.splashing(
+      `${set.modid}:${set.stage3}`,
+      `${set.modid}:${set.stage2}`
+    )
+    event.recipes.create.splashing(
+      `${set.modid}:${set.stage2}`,
+      `${set.modid}:${set.stage1}`
+    )
+
+    // Add deoxidizing by charging recipes
+    event.custom({
+      "type": "createaddition:charging",
+      "input": {
+        "item": `${set.modid}:${set.stage4}`,
+        "count": 1
+      },
+      "result": {
+        "item": `${set.modid}:${set.stage3}`,
+        "count": 1
+      },
+      "energy": 4000,
+      "maxChargeRate": 200
+    })
+    event.custom({
+      "type": "createaddition:charging",
+      "input": {
+        "item": `${set.modid}:${set.stage3}`,
+        "count": 1
+      },
+      "result": {
+        "item": `${set.modid}:${set.stage2}`,
+        "count": 1
+      },
+      "energy": 4000,
+      "maxChargeRate": 200
+    })
+    event.custom({
+      "type": "createaddition:charging",
+      "input": {
+        "item": `${set.modid}:${set.stage2}`,
+        "count": 1
+      },
+      "result": {
+        "item": `${set.modid}:${set.stage1}`,
+        "count": 1
+      },
+      "energy": 4000,
+      "maxChargeRate": 200
+    })
   })
 })
