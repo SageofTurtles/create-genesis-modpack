6e676f6555ec0ede675d62da1b9a5b7d49d30414
SageofTurtles <mkern1200@gmail.com>
2025-07-03 00:30:40 -0500

    Added Packed Up server script file

---
+++ kubejs/server_scripts/thematic_customizations/packedup_server.js
@@ -0,0 +1,21 @@
+ServerEvents.recipes(event => {
+  let bookBundleSet = [
+    { block: 'book_bundle', slab: 'book_bundle_slab', layer: 'book_pile' },
+    { block: 'white_book_bundle', slab: 'white_book_bundle_slab', layer: 'white_book_pile' },
+    { block: 'light_gray_book_bundle', slab: 'light_gray_book_bundle_slab', layer: 'light_gray_book_pile' },
+    { block: 'gray_book_bundle', slab: 'gray_book_bundle_slab', layer: 'gray_book_pile' },
+    { block: 'black_book_bundle', slab: 'black_book_bundle_slab', layer: 'black_book_pile' },
+    { block: 'brown_book_bundle', slab: 'brown_book_bundle_slab', layer: 'brown_book_pile' },
+    { block: 'red_book_bundle', slab: 'red_book_bundle_slab', layer: 'red_book_pile' },
+    { block: 'orange_book_bundle', slab: 'orange_book_bundle_slab', layer: 'orange_book_pile' },
+    { block: 'yellow_book_bundle', slab: 'yellow_book_bundle_slab', layer: 'yellow_book_pile' },
+    { block: 'lime_book_bundle', slab: 'lime_book_bundle_slab', layer: 'lime_book_pile' },
+    { block: 'green_book_bundle', slab: 'green_book_bundle_slab', layer: 'green_book_pile' },
+    { block: 'cyan_book_bundle', slab: 'cyan_book_bundle_slab', layer: 'cyan_book_pile' },
+    { block: 'light_blue_book_bundle', slab: 'light_blue_book_bundle_slab', layer: 'light_blue_book_pile' },
+    { block: 'blue_book_bundle', slab: 'blue_book_bundle_slab', layer: 'blue_book_pile' },
+    { block: 'purple_book_bundle', slab: 'purple_book_bundle_slab', layer: 'purple_book_pile' },
+    { block: 'magenta_book_bundle', slab: 'magenta_book_bundle_slab', layer: 'magenta_book_pile' },
+    { block: 'pink_book_bundle', slab: 'pink_book_bundle_slab', layer: 'pink_book_pile' }
+  ]
+})
