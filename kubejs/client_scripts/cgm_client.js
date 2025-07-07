aaaaff3c9568c471c5c2ffb8c63768e33cf7183a
SageofTurtles <mkern1200@gmail.com>
2025-07-06 23:30:09 -0500

    Modified CGM tooltips to show bound key in text

--- kubejs/client_scripts/cgm_client.js
+++ kubejs/client_scripts/cgm_client.js
@@ -1,15 +1,19 @@
 ItemEvents.tooltip(event => {
+  // Identify keybind
+  const keyMappings = Client.options.keyMappings
+  const useKey = keyMappings.find(km => km.name == 'key.use')
+
   // Dynamite tooltip
-    event.addAdvanced('cgm:grenade', (item, advanced, text) => {
-      text.add(1, [
-        Text.gray('Thrown by holding right-click. Deals damage to nearby entities, but doesn\'t destroy blocks.')
-      ])
-    })
+  event.addAdvanced('cgm:grenade', (item, advanced, text) => {
+    text.add(1, [
+      Text.gold('Thrown by holding ['), Text.yellow(useKey.translatedKeyMessage), Text.gold(']. Deals damage to nearby entities, but doesn\'t destroy blocks.')
+    ])
+  })
 
   // Stun Grenade tooltip
-    event.addAdvanced('cgm:stun_grenade', (item, advanced, text) => {
-      text.add(1, [
-        Text.gray('Thrown by holding right-click. Temporarily blinds and deafens nearby entities.')
-      ])
-    })
+  event.addAdvanced('cgm:stun_grenade', (item, advanced, text) => {
+    text.add(1, [
+      Text.gold('Thrown by holding ['), Text.yellow(useKey.translatedKeyMessage), Text.gold(']. Temporarily blinds and deafens nearby entities.')
+    ])
   })
+})
