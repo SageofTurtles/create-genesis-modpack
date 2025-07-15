3670c20bef68d91a60061751c6123f3460207332
SageofTurtles <mkern1200@gmail.com>
2025-07-14 22:37:04 -0500

    Edited loot tables and crafting recipe
    Removed netherite upgrade templates (will only be obtainable by trading) and echo shards (now only dropped by wardens on death)

---
+++ kubejs/client_scripts/minecraft_client.js
@@ -0,0 +1,15 @@
+ItemEvents.tooltip(event => {
+  // Echo Shard tooltip
+  event.addAdvanced('minecraft:echo_shard', (item, advanced, text) => {
+      text.add(1, [
+        Text.red('Only obtainable by killing a Warden')
+      ])
+    })
+
+  // Netherite Upgrade Smithing Template tooltip
+  event.addAdvanced('minecraft:netherite_upgrade_smithing_template', (item, advanced, text) => {
+      text.add(1, [
+        Text.red('Only obtainable by trading with master Armorer, Toolsmith, or Weaponsmith villagers')
+      ])
+    })
+  })
