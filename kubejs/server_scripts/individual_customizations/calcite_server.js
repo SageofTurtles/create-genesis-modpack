23bafcb6e674335bd2f2cb330e5a146017624b3f
SageofTurtles <mkern1200@gmail.com>
2025-07-31 00:32:38 -0500

    Added Create: Aquatic Ambitions to modpack

--- kubejs/server_scripts/individual_customizations/calcite_server.js
+++ kubejs/server_scripts/individual_customizations/calcite_server.js
@@ -1,10 +1,11 @@
 ServerEvents.recipes(event => {
   event.remove({ id: 'create:filling/calcite' })
+  event.remove({ id: 'create_aquatic_ambitions:compacting/calcite' })
   event.recipes.create.compacting(
     'minecraft:calcite',
     [
-      'minecraft:bone_meal',
-      'minecraft:bone_meal',
+      'create_aquatic_ambitions:calcium_rich_powder',
+      'create_aquatic_ambitions:calcium_rich_powder',
       'kubejs:limesand',
       Fluid.lava(100)
     ]
