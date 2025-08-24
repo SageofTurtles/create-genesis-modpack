1973524ff902c4d7e24061d48dafee1bfd256b1a
SageofTurtles <mkern1200@gmail.com>
2025-08-24 16:40:02 -0500

    Implemented custom method for making Ender Eyes
    Now craft Inert Eye of Ender, then right-click with it on Reinforced Deepslate to convert it into an Eye of Ender.

---
+++ kubejs/client_scripts/ender_eye_client.js
@@ -0,0 +1,24 @@
+let inertEye = 'kubejs:inert_ender_eye'
+let finishedEye = 'minecraft:ender_eye'
+
+BlockEvents.rightClicked('minecraft:reinforced_deepslate', event => {
+  const { player } = event
+  if (event.item.id == inertEye) {
+    event.item.count--
+    if (event.hand == 'main_hand') {
+      player.swing('main_hand', true)
+    } else {
+      player.swing('off_hand', true)
+    }
+    event.cancel()
+  }
+})
+
+JEIEvents.information(event => {
+  event.addItem('minecraft:ender_eye',
+    [
+      'Eyes of Ender can be produced by right-clicking on Reinforced Deepslate with an Inert Eye of Ender in your hand.',
+      '\nUnfortunately, Reinforced Deepslate is only found in the ancient cities of the Deep Dark...'
+    ]
+  )
+})
--- kubejs/server_scripts/individual_customizations/ender_eye_server.js
+++ kubejs/server_scripts/individual_customizations/ender_eye_server.js
@@ -1,17 +1,36 @@
 ServerEvents.recipes(event => {
+  event.remove({ id: 'minecraft:ender_eye' })
   event.shaped(
-    '2x minecraft:ender_eye',
+    'kubejs:inert_ender_eye',
     [
       ' O ',
-      'HES',
+      'CED',
       ' B '
     ],
     {
       O: 'create:powdered_obsidian',
-      H: 'create_aquatic_ambitions:spiky_shell',
+      C: 'create_aquatic_ambitions:calcium_rich_powder',
       E: 'minecraft:ender_pearl',
-      S: 'minecraft:echo_shard',
-      B: 'minecraft:blaze_rod'
+      D: 'createaddition:diamond_grit',
+      B: 'minecraft:blaze_powder'
     }
-  ).id('minecraft:ender_eye')
+  ).id('genesis:inert_ender_eye')
+})
+
+let inertEye = 'kubejs:inert_ender_eye'
+let finishedEye = 'minecraft:ender_eye'
+
+BlockEvents.rightClicked('minecraft:reinforced_deepslate', event => {
+  const { player, block } = event
+  if (event.item.id == inertEye) {
+    event.item.count--
+    player.give(finishedEye)
+    event.server.runCommandSilent(`playsound minecraft:entity.lightning_bolt.thunder block @a ${block.x} ${block.y} ${block.z} 0.7 1`)
+    if (event.hand == 'main_hand') {
+      player.swing('main_hand', true)
+    } else {
+      player.swing('off_hand', true)
+    }
+    event.cancel()
+  }
 })
