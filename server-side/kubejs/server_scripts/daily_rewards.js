5fc03f90e2e8e908530354ea3ee546edf02ec57f
SageofTurtles <mkern1200@gmail.com>
2025-09-11 01:01:31 -0500

    Reworded comments in script for clarity

--- server-side/kubejs/server_scripts/daily_rewards.js
+++ server-side/kubejs/server_scripts/daily_rewards.js
@@ -1,7 +1,8 @@
+// Defines 'date' to be used for both PlayerEvents
 const date = 'BLANK'
 
 PlayerEvents.loggedIn(event => {
-  // Set player's time online back to 0
+  // Set player's time online back to 0 (necessary to set to 0 for new players)
   event.player.persistentData.timeOnline = 0
   
   // Set the current date as string
@@ -13,13 +14,13 @@ PlayerEvents.loggedIn(event => {
 })
 
 PlayerEvents.tick(event => {
-  // Cancel further checks if reward has already been claimed today
+  // Cancel further checks if reward has already been claimed today (to minimize lag)
   if (event.player.persistentData.lastReward === date) return
 
-  // If no reward claimed today, add 1 tick to time online
+  // If no reward was claimed by the player yet today, add 1 tick to time online
   event.player.persistentData.timeOnline++
 
-  // Give reward after 20 minutes online
+  // Give reward after 20 consecutive minutes online
   if (event.player.persistentData.timeOnline >= 24000) {
     event.server.runCommand(`numismatics pay ${event.player.username} 1 COG`)
     event.player.tell([
@@ -30,7 +31,7 @@ PlayerEvents.tick(event => {
       { text: ' as your daily reward.', color: 'light_purple' }
     ])
 
-    // Mark reward as claimed for today's date
+    // Mark reward as claimed for today's date, and reset time online value
     event.player.persistentData.lastReward = date
     event.player.persistentData.timeOnline = 0
     console.log(`${event.player.username} received daily reward for ${event.player.persistentData.lastReward}.`)
