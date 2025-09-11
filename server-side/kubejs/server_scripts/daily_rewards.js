a2e0513b884122ee1b5c1fd403b2cca8aaa6e0ee
SageofTurtles <mkern1200@gmail.com>
2025-09-11 00:54:39 -0500

    Added server-side script for daily rewards
    Awards each player 1 cog (Create: Numismatics) to their account for 20 consecutive minutes (1 in-game day cycle) on the server.  Reward given up to 1 time per real-time day.

---
+++ server-side/kubejs/server_scripts/daily_rewards.js
@@ -0,0 +1,38 @@
+const date = 'BLANK'
+
+PlayerEvents.loggedIn(event => {
+  // Set player's time online back to 0
+  event.player.persistentData.timeOnline = 0
+  
+  // Set the current date as string
+  const today = new Date()
+  const day = today.getDate()
+  const month = today.getMonth() + 1
+  const year = today.getFullYear()
+  date = `${day}-${month}-${year}`
+})
+
+PlayerEvents.tick(event => {
+  // Cancel further checks if reward has already been claimed today
+  if (event.player.persistentData.lastReward === date) return
+
+  // If no reward claimed today, add 1 tick to time online
+  event.player.persistentData.timeOnline++
+
+  // Give reward after 20 minutes online
+  if (event.player.persistentData.timeOnline >= 24000) {
+    event.server.runCommand(`numismatics pay ${event.player.username} 1 COG`)
+    event.player.tell([
+      { text: 'Thanks for playing, ', color: 'light_purple' },
+      { text: event.player.username, color: 'aqua' },
+      { text: '! You\'ve received ', color: 'light_purple' },
+      { text: '1 cog', color: 'aqua' },
+      { text: ' as your daily reward.', color: 'light_purple' }
+    ])
+
+    // Mark reward as claimed for today's date
+    event.player.persistentData.lastReward = date
+    event.player.persistentData.timeOnline = 0
+    console.log(`${event.player.username} received daily reward for ${event.player.persistentData.lastReward}.`)
+  }
+})
