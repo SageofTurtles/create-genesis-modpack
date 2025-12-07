70311811770d7fad063bd8c80294fcc7102a18b9
SageofTurtles <mkern1200@gmail.com>
2025-12-06 18:55:47 -0600

    Renamed villager trade script file

--- common/kubejs/server_scripts/villager_trades_server.js
+++
@@ -1,6 +0,0 @@
-MoreJSEvents.villagerTrades((event) => {
-    const toolsmithTrade = event.addTrade('toolsmith', 5, ['minecraft:netherite_scrap', TradeItem.of('minecraft:diamond', 24, 48)], 'minecraft:netherite_upgrade_smithing_template');
-    toolsmithTrade.maxUses(2);
-    toolsmithTrade.villagerExperience(45);
-    toolsmithTrade.priceMultiplier(0.1);
-  });
