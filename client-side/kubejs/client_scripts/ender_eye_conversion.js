8fec81213c63b763694dc383258280b07ac8bb16
SageofTurtles <mkern1200@gmail.com>
2025-12-29 21:02:43 -0600

    Improved conversion script for Ender Eyes
    Now simply replaces the stack in the player's hand when converted, instead of removing and giving a single Ender Eye (prevents items being lost if no inventory space is available)

--- client-side/kubejs/client_scripts/ender_eye_conversion.js
+++ client-side/kubejs/client_scripts/ender_eye_conversion.js
@@ -3,13 +3,14 @@ let finishedEye = 'minecraft:ender_eye'
 
 BlockEvents.rightClicked('minecraft:reinforced_deepslate', event => {
   const { player } = event
-  if (event.item.id == inertEye) {
-    event.item.count--
-    if (event.hand == 'main_hand') {
-      player.swing('main_hand', true)
-    } else {
-      player.swing('off_hand', true)
-    }
-    event.cancel()
+  if (event.item.id != inertEye) return
+  let quantity = event.item.count
+  if (event.hand == 'main_hand') {
+    player.swing('main_hand', true)
+    player.setHeldItem("main_hand", Item.of(finishedEye, quantity))
+  } else {
+    player.swing('off_hand', true)
+    player.setHeldItem("off_hand", Item.of(finishedEye, quantity))
   }
+  event.cancel()
 })
--- common/kubejs/server_scripts/ender_eye_conversion.js
+++ common/kubejs/server_scripts/ender_eye_conversion.js
@@ -3,15 +3,15 @@ let finishedEye = 'minecraft:ender_eye'
 
 BlockEvents.rightClicked('minecraft:reinforced_deepslate', event => {
   const { player, block } = event
-  if (event.item.id == inertEye) {
-    event.item.count--
-    player.give(finishedEye)
-    event.server.runCommandSilent(`playsound minecraft:entity.lightning_bolt.thunder block @a ${block.x} ${block.y} ${block.z} 0.7 1`)
-    if (event.hand == 'main_hand') {
-      player.swing('main_hand', true)
-    } else {
-      player.swing('off_hand', true)
-    }
-    event.cancel()
+  if (event.item.id != inertEye) return
+  let quantity = event.item.count
+  event.server.runCommandSilent(`playsound minecraft:entity.lightning_bolt.thunder block @a ${block.x} ${block.y} ${block.z} 0.7 1`)
+  if (event.hand == 'main_hand') {
+    player.swing('main_hand', true)
+    player.setHeldItem("main_hand", Item.of(finishedEye, quantity))
+  } else {
+    player.swing('off_hand', true)
+    player.setHeldItem("off_hand", Item.of(finishedEye, quantity))
   }
+  event.cancel()
 })
